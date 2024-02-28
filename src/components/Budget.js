015b48d8c23044d1354bd7a9049c1be263326505
Krut Patel <krut.patel93@gmail.com>
2024-02-27 22:38:16 -0500

    dinner

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,16 +1,26 @@
 import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
+
+
 const Budget = () => {
-    const { dispatch, budget } = useContext(AppContext);
+    const { dispatch, budget, expenses } = useContext(AppContext);
+    const totalExpenses = expenses.reduce((total, item) => {
+        return (total = total + item.cost);
+    }, 0);
+    console.log(totalExpenses);
     const [newBudget, setNewBudget] = useState(budget);
     const handleBudgetChange = (event) => {
         setNewBudget(event.target.value);
-        console.log(event)
-        dispatch({
+        
+        if(newBudget <= totalExpenses){
+            alert("You can't reduce the budget lower than spending");
+        }
+        else
+        {dispatch({
             type: 'SET_BUDGET',
             payload: event.target.value,
-        });
+        });}
     }
     return (
 <div className='alert alert-secondary'>
